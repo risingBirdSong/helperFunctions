251fbb6574f7996d366c7c4e267f594719fbd690
Peter <heregoingthere@outlook.com>
2020-07-08 17:24:21 -0700

    Delete makeManyAnon_Funcs.ts

--- makeManyAnon_Funcs.ts
+++
@@ -1,5 +0,0 @@
-let stringnames = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-let mapped = stringnames.map(function (name, n) {
-    //@ts-ignore
-  return this[name] = function () { return  `test of ${name}`};
-});
